b39b00d9bae83800d97c305c6b081b4ce5ff1855
刘作栋 <657793814@qq.com>
2025-09-05 10:17:53 +0800

    optional_code

--- src/main/java/com/liuzd/soft/controller/IndexController.java
+++ src/main/java/com/liuzd/soft/controller/IndexController.java
@@ -1,22 +1,15 @@
 package com.liuzd.soft.controller;
 
-import com.liuzd.soft.annotation.LogAnnotation;
-import com.liuzd.soft.enums.RetEnums;
 import com.liuzd.soft.service.LoginService;
-import com.liuzd.soft.service.Strategies;
 import com.liuzd.soft.service.impl.ServiceCall;
-import com.liuzd.soft.service.impl.StrategiesFactory;
 import com.liuzd.soft.vo.login.LoginReq;
 import com.liuzd.soft.vo.login.RefreshTokenReq;
 import com.liuzd.soft.vo.login.UnLoginReq;
 import com.liuzd.soft.vo.rets.ResultMessage;
 import jakarta.servlet.http.HttpServletRequest;
-import jodd.util.StringUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -43,21 +36,7 @@ public class IndexController {
         //http://localhost:8080/call_service
         return ResultMessage.success(serviceCall.callService("saas-saler-admin-prod", "/hello_world"));
     }
-
-    @RequestMapping(path = "/test")
-    @LogAnnotation
-    public ResultMessage<Object> index(@RequestParam(value = "name") String name) {
-
-        if (StringUtil.isBlank(name)) {
-            return ResultMessage.fail(RetEnums.PARAMETER_NOT_VALID.getCode(), RetEnums.PARAMETER_NOT_VALID.getMessage());
-        }
-        Strategies strategies = StrategiesFactory.strategiesMap.get(StrategiesFactory.GLOBAL_STRATEGIES_PREFIX + name);
-        if (ObjectUtils.isEmpty(strategies)) {
-            return ResultMessage.fail(RetEnums.PARAMETER_NOT_VALID.getCode(), RetEnums.PARAMETER_NOT_VALID.getMessage());
-        }
-        strategies.doSomething();
-        return ResultMessage.success("exec success");
-    }
+    
 
     @RequestMapping(path = "/refresh_token")
     public ResultMessage<Object> refreshToken(
